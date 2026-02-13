af3578d0966c3ac9d030dd253738ff4271e8a066
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-02-13 22:27:46 +0100

    EAI-13: Implement la logique de l' ingestion

--- data-processing/airflow/tasks/bronze_ingestion.py
+++ data-processing/airflow/tasks/bronze_ingestion.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-def run_ingestion():
+def download_dataset():
     load_dotenv()
     
     # Set environment variables from .env or kaggle.json
@@ -12,18 +12,19 @@ def run_ingestion():
     api = KaggleApi()
     api.authenticate()
     print("Authentification réussie !")
-    # base_dir = os.path.dirname(os.path.abspath(__file__))
-    # bronze_path = os.path.join(base_dir, "../data/bronze/plant-doc")
-    # dataset_id = "abdulhasibuddin/plant-doc-dataset"
+    
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    bronze_path = os.path.join(base_dir, "../data/bronze/plant-doc")
+    dataset_id = "abdulhasibuddin/plant-doc-dataset"
 
  
 
-    # # Téléchargement et Décompression
-    # print(f"Téléchargement du dataset {dataset_id} en cours...")
+    # Téléchargement et Décompression
+    print(f"Téléchargement du dataset {dataset_id} en cours...")
     
-    # api.dataset_download_files(dataset_id, path=bronze_path, unzip=True)
+    api.dataset_download_files(dataset_id, path=bronze_path, unzip=True)
     
-    # print(f"Ingestion Bronze terminée. Fichiers stockés dans : {bronze_path}")
+    print(f"Ingestion Bronze terminée. Fichiers stockés dans : {bronze_path}")
 
 if __name__ == "__main__":
-    run_ingestion()
+    download_dataset ()
