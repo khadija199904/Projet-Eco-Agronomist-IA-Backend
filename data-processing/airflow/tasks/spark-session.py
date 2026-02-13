f6b35973f1bab8a5c6918a6772f946a8f9e27e36
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-02-13 22:33:01 +0100

    EAI-13: delete files

--- data-processing/airflow/tasks/spark-session.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
-def get_spark_session(app_name):
-    return SparkSession.builder \
-        .appName(app_name) \
-        .config("spark.sql.parquet.compression.codec", "snappy") \
-        .getOrCreate()
