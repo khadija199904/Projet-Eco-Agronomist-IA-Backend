9db8fcd8bf06e84f9d9dc37ed6bd4016d3bd150c
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-02-17 14:23:13 +0000

    EAI-13 : dags pour tache standarization et triger principale dag

---
+++ data-processing/airflow/dags/roboflow-prep-dag.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from datetime import datetime
+from tasks.standardize_roboflow import standardize_roboflow
+
+with DAG('roboflow_standardization_pipeline', 
+         start_date=datetime(2026, 2, 17), 
+         schedule_interval=None) as dag:
+
+    
+    t1 = PythonOperator(
+        task_id='standardize_roboflow_files',
+        python_callable=standardize_roboflow
+    )
+
+    # Déclencher le DAG principal automatiquement après
+    t2 = TriggerDagRunOperator(
+        task_id='trigger_main_pipeline',
+        trigger_dag_id='medallion_plant_disease_v1' ,
+        wait_for_completion=True,  
+        poke_interval=30,          
+        deferrable=True
+    )
+
+    t1 >> t2
