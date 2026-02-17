a1d231f66664b4fdf9d805032c41a9513a344752
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-02-17 14:19:04 +0000

    EAI-13 : modifier la fonction d'ingestion robflow data

--- data-processing/airflow/tasks/bronze_ingestion.py
+++ data-processing/airflow/tasks/bronze_ingestion.py
@@ -1,32 +1,31 @@
 import os
-from dotenv import load_dotenv
 import shutil
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 def ingest_dataset():
-    load_dotenv()
-    
-    # Set environment variables from .env or kaggle.json
-    os.environ['KAGGLE_USERNAME'] = os.getenv('KAGGLE_USERNAME')
-    os.environ['KAGGLE_KEY'] = os.getenv('KAGGLE_KEY')
+
     
     api = KaggleApi()
-    api.authenticate()
-    print("Authentification réussie !")
+    try:
+        api.authenticate()
+        print("Authentification réussie via Docker Env !")
+    except Exception as e:
+        print(f"Erreur : Vérifiez que KAGGLE_USERNAME et KAGGLE_KEY sont bien dans Docker. {e}")
+        raise
     
     base_dir = os.path.dirname(os.path.abspath(__file__))
     bronze_path = os.path.join(base_dir, "../data/bronze/plant-doc")
     dataset_id = "yusufmurtaza01/plantdoc-object-detection-dataset"
 
- 
-
     # Téléchargement et Décompression
     print(f"Téléchargement du dataset {dataset_id} en cours...")
     
     api.dataset_download_files(dataset_id, path=bronze_path, unzip=True)
-    
+
+    print(f"Ingestion depuis kaggle terminée")
      
-    RAW_ROBO = "/opt/airflow/data/raw/"
+     #---------------Roboflow-------------------------
+    RAW_ROBO = "/opt/airflow/data/raw/roboflow_standardized"
     BRONZE_ROBO = "/opt/airflow/data/bronze/roboflow"
     
     print("--- Ingestion Bronze : Transfert Roboflow ---")
@@ -41,6 +40,8 @@ def ingest_dataset():
     
     shutil.copytree(RAW_ROBO, BRONZE_ROBO)
     print(f"Données Roboflow copiées avec succès vers {BRONZE_ROBO}")
-    print(f"Ingestion Bronze terminée. Fichiers stockés dans : {bronze_path}")
+    print(f"Ingestion Bronze Totale terminée.")
+
+
 if __name__ == "__main__":
     ingest_dataset ()
