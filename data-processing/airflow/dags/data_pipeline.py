ea79cd4a881599ec327157b5a2a0f31c670d3561
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-02-17 02:42:05 +0000

    EAI-13: modifier pipeline dag

--- data-processing/airflow/dags/data_pipeline.py
+++ data-processing/airflow/dags/data_pipeline.py
@@ -1,21 +1,27 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
 from datetime import datetime
 import os
 import sys
 
 # Ensure parent 'airflow' folder is on sys.path so we can import sibling 'tasks' package
-current_dir = os.path.dirname(__file__)
-airflow_dir = os.path.dirname(current_dir)
-if airflow_dir not in sys.path:
-    sys.path.insert(0, airflow_dir)
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
+
 
 from tasks.bronze_ingestion import download_dataset
-SILVER_SCRIPT = os.path.join(airflow_dir, "tasks", "tasks.silver_transformation ")
+from tasks.kaggle_uploader import upload_to_kaggle
+
+from airflow.operators.python import PythonOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+SILVER_SCRIPT = os.path.join(ROOT_DIR, "tasks", "silver.py")
 
 
-with DAG('medallion_agadir_plantdoc', start_date=datetime(2024, 1, 1), schedule=None,catchup=False) as dag:
+
+with DAG('medallion_plantdoc', start_date=datetime(2024, 1, 1), schedule=None,catchup=False) as dag:
 
     #  BRONZE : Ingestion brute
     ingest_task = PythonOperator(
@@ -25,10 +31,19 @@ with DAG('medallion_agadir_plantdoc', start_date=datetime(2024, 1, 1), schedule=
     
     # SILVER : Filtrage des plantes d'Agadir (à implémenter)
     silver_task = SparkSubmitOperator(
-        task_id='pyspark_silver_filtering',
-        application=SILVER_SCRIPT,
-        conn_id='spark_default'
+    task_id='pyspark_silver_filtering',
+    conn_id='spark_default',
+    application='/opt/airflow/tasks/silver.py'
+)
+        
+    
+    #  UPLOAD KAGGLE
+    upload_task = PythonOperator(
+        task_id='upload_result_to_kaggle',
+        python_callable=upload_to_kaggle
     )
+
+    # Enchaînement des tâches
+    ingest_task >> silver_task >> upload_task
     
     
-    ingest_task  >> silver_task 
