fada3565cb422cc0e4e6bd04afa36d7a54673ce9
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-03-10 22:00:10 +0000

    modifier __init__.py

--- src/api/v1/routers/__init__.py
+++ src/api/v1/routers/__init__.py
@@ -1,6 +1,6 @@
 from .auth import router as auth
 from .diagnostic import router as diagnostic
-from .lots import router as lots
+from .production import router as production
 from .valorisation import router as valorisation
 
-__all__ = ["auth", "diagnostic", "lots", "valorisation"]
+__all__ = ["auth", "diagnostic", "production", "valorisation"]
