4ca7cb470c4b7487475f009d7980ffe0bc84e16c
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-03-11 10:38:48 +0000

    EAI-8: Ajout systeme d'ogranisation (crud,shema, route)

---
+++ src/api/v1/schemas/organization_schema.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import Optional
+from src.database.models.enums import OrgType
+
+class OrganizationBase(BaseModel):
+    name: str
+    type: OrgType
+    address: Optional[str] = None
+    is_certified: Optional[bool] = False
+
+class OrganizationCreate(OrganizationBase):
+    pass
+
+class OrganizationUpdate(BaseModel):
+    name: Optional[str] = None
+    type: Optional[OrgType] = None
+    address: Optional[str] = None
+    is_certified: Optional[bool] = None
+
+class OrganizationResponse(OrganizationBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class OrganizationListResponse(BaseModel):
+    total: int
+    organizations: list[OrganizationResponse]
