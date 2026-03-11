ec4f3435ece5b8c0cffcfe3c72453ad3a747124d
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-03-11 14:46:48 +0000

    EAI-8: Correcter les importations(valorisation)

--- src/api/v1/routers/valorisation.py
+++ src/api/v1/routers/valorisation.py
@@ -13,7 +13,7 @@ from src.api.v1.schemas.valorisation_schema import (
 )
 from src.api.v1.crud import valorisation_crud, diagnostic_crud
 from src.api.v1.services import diagnostic_service
-from src.database.models.diagnostics import DiagnosticProduct
+from src.database.models.diagnostics_table import DiagnosticProduct
 
 router = APIRouter()
 
