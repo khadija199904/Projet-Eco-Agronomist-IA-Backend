bfd3e816d617f5bc1f1cf7d618afed44ee06fce1
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-03-11 23:32:11 +0000

    EAI-8: correcter les importations

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -2,6 +2,7 @@
 from .users import User, Admin, Agriculteur, QualityControl, Consumer
 from .organization_table import Organization
 from .production_table import LotRecolte
-from .diagnostics_table import PlantDiagnostic,TreatmentRAG, DiagnosticProduct, ConsumerDiagnostic
+from .diagnostics_table import UniversalDiagnostic
+from .ordonnacesIA import TreatmentRAG
 from .valorisation_table import TraitementStation
 from .consommation_table import ProduitFini
